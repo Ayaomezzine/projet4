e55fc7523c97b169b07e2b066a6d9478111b6aca
Ayaomezzine <80257196+Ayaomezzine@users.noreply.github.com>
2022-05-05 08:30:01 +0100

    Update and rename analyse.sh to help.sh

---
+++ help.sh
@@ -0,0 +1,12 @@
+ceci est un script qui permet d'analyser /etc/passwd et afficher son contenu 
+sous forme plus lisible;
+Utilisez les options suivantes:
+-g : afficher un menu textuel et gerer les fonctionalités de façon graphiques
+-v : afficher le nom des auteurs et version de code 
+-m : afficher un menu textuel 
+-n : afficher le nombre d'utlisateurs
+-p : afficher les repertoires personnels des utilisateurs
+-G : utilisateurs appartenant au meme groupe
+-t : type d'utilisateurs 
+
+
